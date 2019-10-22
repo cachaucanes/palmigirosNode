afe1f01b93e2377e6c7f7163ab8b45a4331c8c88
cachaucanes23 <cachaucanes23@gmail.com>
2019-10-22 17:56:33 -0500

    Permisos model

---
+++ src/models/permisos.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize'
+import { sequelize } from '../database'
+
+const Permisos = sequelize.define('permisos', {
+  idPermisos : {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: false
+  },
+  descripcion: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  tabla: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+})
+
+export default Permisos
